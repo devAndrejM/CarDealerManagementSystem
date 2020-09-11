24fc0b5709d69993860aa8341f90bb687f145493
Andrej M <andrej-1993@hotmail.com>
2020-09-11 16:23:55 +0200

    New GUI,reset and exit work

--- src/main/java/com/andrej/App.java
+++ src/main/java/com/andrej/App.java
@@ -1,108 +1,145 @@
 package com.andrej;
 
+
 import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-public class App {
-    private JPanel CarSystemManagement;
-    private JPanel CustomerDetails;
-    private JPanel Accessories;
-    private JPanel CarDetails;
-    private JCheckBox Stereo;
-    private JCheckBox Leather;
-    private JCheckBox GPS;
-    private JCheckBox Standard;
-    private JCheckBox Modified;
-    private JLabel ID;
-    private JLabel FirstName;
-    private JLabel LastName;
-    private JLabel Address;
-    private JLabel PostCode;
+public class App implements ActionListener{
+    private JPanel panelContainer;
+    private JPanel MainPanel;
+    private JPanel ButtonPanel;
+    private JPanel User;
+    private JButton publishButton;
+    private JButton resetButton;
+    private JButton exitButton;
+    private JPanel Admin;
+    private JButton publishButtonAdmin;
+    private JButton resetButtonAdmin;
+    private JButton editButtonAdmin;
+    private JButton exitButtonAdmin;
+    private JButton FindButtonAdmin;
+    JPanel CustomerDetailsPanel;
+    private JLabel CustomerID;
+    JTextField idTxt;
+    private JLabel FName;
+    JTextField fNameTxt;
+    private JLabel LName;
+    JTextField lNameTxt;
     private JLabel Town;
-    private JTextField IDtxt;
-    private JTextField FNametxt;
-    private JTextField LNametxt;
-    private JTextField Addresstxt;
-    private JTextField ZIPtxt;
-    private JTextField Towntxt;
-    private JComboBox SelectCar;
-    private JTextField Yeartxt;
-    private JTextField Pricetxt;
-    private JLabel Year;
-    private JLabel Price;
-    private JButton Exit;
-    private JButton Reset;
-    private JButton Apply;
-    private JButton Find;
-    private JCheckBox ElectricWindows;
-    private JLabel Mileage;
-    private JTextField Mileagetxt;
-    private JLabel CarDetailsLabel;
-    private JLabel CustomerDetailsLabel;
-    private JTextField CarModeltxt;
+    JTextField townTxt;
+    private JLabel Postcode;
+    JTextField zipTxt;
+    private JLabel Address;
+    JTextField addressTxt;
+    private JPanel AccessoriesPanel;
+    private JCheckBox stereoCheckBox;
+    private JCheckBox modifiedCheckBox;
+    private JCheckBox customizedCheckBox;
+    private JCheckBox electricWindowsCheckBox;
+    private JCheckBox GPSCheckBox;
+    private JCheckBox leatherCheckBox;
+    JPanel CarDetails;
+    private JComboBox comboBox1;
     private JLabel CarModel;
+    JTextField modelTxt;
+    private JLabel Price;
+    JTextField priceTxt;
+    private JLabel Mileage;
+    JTextField mileageTxt;
+    private JLabel Year;
+    JTextField yearTxt;
+    private JPanel Welcome;
+    private JPanel LoginForm;
+    private JButton loginButton;
+    private JButton createNewAccountButton;
+    private JTextField userTxt;
+    private JPasswordField passTxt;
+    private JLabel Username;
+    private JLabel Password;
+    private JButton exitButtonWelcome;
+    private JLabel Greeting;
 
-    private JFrame frame;
+
 
     public App() {
-        final JFrame exitFrame = new JFrame("Exit");
-        Exit.addActionListener(new ActionListener() {
-            public void actionPerformed(ActionEvent e) {
-                if(JOptionPane.showConfirmDialog(exitFrame, "Confirm if you want to exit", "Car Dealer Management System", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION)
-                {
-                    System.exit(0);
-                }
-            }
-        });
-        final JFrame resetFrame = new JFrame("Reset");
-        Reset.addActionListener(new ActionListener() {
-            JTextField clearText = null;
-            public void actionPerformed(ActionEvent e) {
-                if (JOptionPane.showConfirmDialog(resetFrame, "Confirm if you want to reset", "Reset", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
+        exitButton.setActionCommand("Exit");
+        exitButton.addActionListener(this);
+        exitButtonAdmin.setActionCommand("Exit");
+        exitButtonAdmin.addActionListener(this);
+        exitButtonWelcome.setActionCommand("Exit");
+        exitButtonWelcome.addActionListener(this);
 
-                    for (Component c : CustomerDetails.getComponents()) {
-                        if (c.getClass().toString().contains("javax.swing.JTextField")) {
-                            clearText = (JTextField) c;
-                            clearText.setText("");
-                            IDtxt.setText("");
-                            FNametxt.setText("");
-                            LNametxt.setText("");
-                            Addresstxt.setText("");
-                            ZIPtxt.setText("");
-                            Towntxt.setText("");
-                        }
+        resetButton.setActionCommand("Reset");
+        resetButton.addActionListener(this);
+        resetButtonAdmin.setActionCommand("Reset");
+        resetButtonAdmin.addActionListener(this);
 
-                    }
-                    for (Component c : CarDetails.getComponents()) {
-                        if (c.getClass().toString().contains("javax.swing.JTextField")) {
-                            clearText = (JTextField) c;
-                            clearText.setText("");
-                            Yeartxt.setText("");
-                            Pricetxt.setText("");
-                            Mileagetxt.setText("");
-                            CarModeltxt.setText("");
-                        }
 
-                    }
-                }
-            }
-        });
-    }
+
+        };
+        
+
 
     public static void main(String[] args) {
-        JFrame frame = new JFrame("Car Dealer Management System");
-        frame.setContentPane(new App().CarSystemManagement);
+        JFrame frame = new JFrame("Car System Management");
+
+        frame.setContentPane(new App().panelContainer);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        frame.pack();
+        frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+        frame.pack();
 
     }
 
 
+    public void actionPerformed(ActionEvent e) {
+        if (e.getActionCommand().equals("Reset")) {
+            JFrame resetFrame = new JFrame("Reset");
+            int response = JOptionPane.showConfirmDialog(resetFrame, "Confirm if you want to reset",
+                    "Reset", JOptionPane.YES_NO_OPTION);
+            JTextField clearText;
 
-    private void createUIComponents() {
-        // TODO: place custom component creation code here
+            if (response == JOptionPane.YES_OPTION) {
+
+                for (Component c : CustomerDetailsPanel.getComponents()) {
+                    if (c.getClass().toString().contains("javax.swing.JTextField")) {
+                        clearText = (JTextField) c;
+                        clearText.setText("");
+                        idTxt.setText("");
+                        fNameTxt.setText("");
+                        lNameTxt.setText("");
+                        townTxt.setText("");
+                        zipTxt.setText("");
+                        addressTxt.setText("");
+
+                    }
+
+                }
+                for (Component c : CarDetails.getComponents()) {
+                    if (c.getClass().toString().contains("javax.swing.JTextField")) {
+                        clearText = (JTextField) c;
+                        clearText.setText("");
+                        modelTxt.setText("");
+                        priceTxt.setText("");
+                        mileageTxt.setText("");
+                        yearTxt.setText("");
+                    }
+                }
+            } else if (response == JOptionPane.NO_OPTION) {
+                resetFrame.dispose();
+            } else if (response == JOptionPane.CLOSED_OPTION) {
+                resetFrame.dispose();
+            }
+        } else if (e.getActionCommand().equals("Exit")) {
+            JFrame frame;
+            frame = new JFrame("Exit");
+
+            if (JOptionPane.showConfirmDialog(frame, "Confirm if you want to exit", "Exit", JOptionPane.YES_NO_OPTION) == JOptionPane.YES_NO_OPTION) {
+                System.exit(0);
+            }
+        }
     }
-}
+    }
+
